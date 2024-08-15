455ca513d99f1bb378f88d389d2478647ddd5c72
cavalenciad <cavalenciad@outlook.com>
2024-08-15 18:46:58 -0500

    Create get service

---
+++ src/app/services/cities.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CitiesService {
+
+  private apiUrl = 'http://localhost:3000/';
+
+  constructor(private http:HttpClient) { }
+
+  getCities() {
+    return this.http.get(this.apiUrl+'cities')
+  }
+}
