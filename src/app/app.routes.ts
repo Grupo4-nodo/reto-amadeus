bcf20a61ded329b6cb60006f35c94aac834838d8
David Carmona <davidcarmonab4@gmail.com>
2024-08-18 17:01:14 -0500

    feat(questions): add AMADEUS-QUESTIONS component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AmadeusQuestionComponent } from './pages/amadeus-question/amadeus-question.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'amadeus-questions', component:AmadeusQuestionComponent}
+];
