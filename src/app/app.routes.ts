0370434dda6d6f53a4c2dc7f60a92b0ca5c4622c
cavalenciad <cavalenciad@outlook.com>
2024-08-19 15:53:32 -0500

    refactor(db and config): update info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CitiesComponent } from './pages/cities/cities.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'cities', component:CitiesComponent}
+];
