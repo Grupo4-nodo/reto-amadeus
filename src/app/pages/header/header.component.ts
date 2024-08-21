8a040440b6261bb79b7cea594718f924b62041ba
lorenqg <lorenquiceno@gmail.com>
2024-08-21 17:06:44 -0500

    feat(header): add structure html, styles and logic to logout

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,12 +1,23 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgIf],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
 
+  isLogged(): boolean {
+    return !!sessionStorage.getItem('email');
+  }
+
+  logout() {
+    sessionStorage.removeItem('email');
+    this.router.navigate(['/']);
+  }
 }
