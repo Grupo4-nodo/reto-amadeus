a6d74b9491e46c7ef2b42c60e726e9091c7c06b4
Edwin Restrepo <egrestrepa@eafit.edu.co>
2024-08-21 13:03:58 -0500

    fix (merge):solve conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { AmadeusQuestionComponent } from './pages/amadeus-question/amadeus-question.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CitiesComponent } from './pages/cities/cities.component';
+import { QuestionsComponent } from './pages/questions/questions.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'amadeus-questions', component: AmadeusQuestionComponent },
   {path: '', component:HomeComponent},
-  {path: 'cities', component:CitiesComponent}
+  {path: 'cities', component:CitiesComponent},
+  {path: 'questions', component:QuestionsComponent}
+
 ];
